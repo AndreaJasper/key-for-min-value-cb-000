ecca3bda288e6b1bebbdd76cbc13393ce8da9c3f
Andrea Jasper <andrea.nicole.jasper@gmail.com>
2019-04-20 23:50:40 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -6,7 +6,7 @@ def key_for_min_value(name_hash)
 
   lowest = ""
   ikea.collect do |name, amount|
-    if amount ==
+    if amount < 85 && amount < 450
       lowest = name
     else
       return nil
