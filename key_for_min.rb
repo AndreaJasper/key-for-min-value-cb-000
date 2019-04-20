c7185e7a328cebd4c3942bd9833d54824f9a948e
Andrea Jasper <andrea.nicole.jasper@gmail.com>
2019-04-20 23:55:43 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,15 +2,9 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-  ikea = {:chair => 25, :table => 85, :matress => 450}
-
-  lowest = ""
-  ikea.collect do |name, amount|
-    if amount == amount < 85 && amount < 450
-      lowest = name
-    else
-      return nil
-    end
-  end
-  lowest
+  if name_hash.empty?
+    retun nil
+  else
+  lowest = name_hash.reduce { |x, y| x.last > y.last ? y : x }.first
+    return lowest
 end
